d1201949426d84021bd5f1dffe8beb17700d7d70
Johnathan <107622965+Johnathank78@users.noreply.github.com>
2025-08-27 22:27:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -109,19 +109,15 @@ async function createListenKey(request) {
 }
 
 async function keepAlive(request) {
-  const { apiKey, listenKey } = await request.json()
-  const resp = await fetch("https://api.binance.com/api/v3/userDataStream", {
+  const { apiKey, listenKey } = await request.json();
+  const resp = await fetch(`https://api.binance.com/api/v3/userDataStream?listenKey=${encodeURIComponent(listenKey)}`, {
     method: "PUT",
-    headers: {
-      "X-MBX-APIKEY": apiKey,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ listenKey })
-  })
+    headers: { "X-MBX-APIKEY": apiKey }
+  });
   return new Response('{"ok":true}', {
     status: resp.ok ? 200 : resp.status,
     headers: { "Content-Type": "application/json" }
-  })
+  });
 }
 
 async function proxySigned(request) {
